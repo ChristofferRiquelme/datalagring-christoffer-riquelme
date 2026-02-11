437ce163d52240026d068dee619085a11599253d
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2026-02-11 14:57:10 +0100

    Tested endpoints with POSTMAN

--- Datalagring.Application/obj/Debug/net10.0/Datalagring.Application.AssemblyInfo.cs
+++ Datalagring.Application/obj/Debug/net10.0/Datalagring.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datalagring.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b5dad7f181d91a63a0f1bf908724b777f4edd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7724c7179b23523c4a8aa0c7e75e3f4f98fd5b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datalagring.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datalagring.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Datalagring.Tests/obj/Debug/net10.0/Datalagring.Tests.AssemblyInfo.cs
+++ Datalagring.Tests/obj/Debug/net10.0/Datalagring.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datalagring.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78bd201a951af64b5e022f4c41a691e1427a0d61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7724c7179b23523c4a8aa0c7e75e3f4f98fd5b4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datalagring.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datalagring.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
